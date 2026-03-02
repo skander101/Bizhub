a5f38296f09ea4480cd30d9e5b1661b1723e5614
skander101 <selmiskander0.8@gmail.com>
2026-03-02 23:54:00 +0100

    Removed useless folder

--- Bizhub-User-Java/src/main/java/com/bizhub/model/services/marketplace/payment/PaymentProvider.java
+++
@@ -1,7 +0,0 @@
-package com.bizhub.model.services.marketplace.payment;
-
-import com.bizhub.model.marketplace.CommandeJoinProduit;
-
-public interface PaymentProvider {
-    PaymentResult createCheckout(CommandeJoinProduit commande) throws Exception;
-}
